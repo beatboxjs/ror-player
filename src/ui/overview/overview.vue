1c81afc791728757f530a8c73ed6c3539cc1d0d8
Tim <user@server.fake>
2022-12-12 21:11:22 +0000

    Buttons

--- src/ui/overview/overview.vue
+++ src/ui/overview/overview.vue
@@ -2,34 +2,48 @@
 	<Compatibility />
 	<Update />
 
-	<span class="bb-overview-toggle-patternList navbar-light d-inline-block d-md-none" @click="togglePatternList()">
-		<b-button class="navbar-toggler"><span class="navbar-toggler-icon" /></b-button>
-	</span>
-
 	<span class="bb-overview-help">
 		<Help />
 	</span>
 
-{{activeTab}}
-	<b-tabs v-model="activeTab">
-		<b-tab title="Listen" key="1">
+	<div class="container-fluid d-flex flex-column h-100">
+		<div class="row m-2 g-5">
+			<span class="navbar-light d-inline-block d-md-none" @click="togglePatternList()">
+				<b-button class="navbar-toggler"><span class="navbar-toggler-icon" /></b-button>
+			</span>
+			<button type="button" class="btn btn-light"
+				:class="{active: activeTab==0}"
+				@click="activeTab=0"
+			>Listen</button> 
+			<button type="button" class="btn btn-light"
+				:class="{active: activeTab==1}"
+				@click="activeTab=1">Compose</button>
+			<button type="button" class="btn btn-light"
+				v-if="editorTab"
+				:class="{active: activeTab==2}"
+				@click="activeTab=2"
+			>{{editorTab.title}}
+			<a href="" @click="closeTab()">×</a>
+			</button>
+		</div>
+
+		<div v-if="activeTab == 0" class="flex-grow-1">
 			<Listen />
-		</b-tab>
-		<b-tab title="Compose" key="2">
+		</div>
+		<div v-if="activeTab == 1" class="flex-grow-1">
 			<StateProvider>
-				<Compose v-if="activeTab == 1" />
+				<Compose />
 			</StateProvider>
-		</b-tab>
-		<b-tab v-if="editorTab" key="3">
-			<template #title>{{editorTab.title}} <a href="" @click="closeTab()">×</a></template>
+		</div>
+		<div v-if="activeTab == 2" class="flex-grow-1">
 			<StateProvider>
 				<PatternPlayer class="m-4"
 					:tuneName="editorTab.content.tuneName" 
 					:pattern-name="editorTab.content.patternName" 
 					:readonly="true" />	
 			</StateProvider>
-		</b-tab>
-	</b-tabs>
+		</div>
+	</div>
 
 	<div class="bb-cover" @click="togglePatternList()"></div>
 </div>
