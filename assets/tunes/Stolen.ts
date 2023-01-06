ac855464a1f6ed560bb3c4e2bcb87e77692d75cf
Birch <119069399+BirchBirchBirch@users.noreply.github.com>
2023-01-06 10:00:56 +0000

    Update Stolen.ts (#14)
    Fixing timing issues, added 4 Hits

--- assets/tunes/Stolen.ts
+++ assets/tunes/Stolen.ts
@@ -1,21 +1,21 @@
 export default {
     categories: ["standard", "common", "tricky"],
-    time: 12,
     sheet: undefined,
     description: require("./stolen.md").default,
     video: undefined,
     patterns: {
         Tune: {
             loop: true,
-            ls: "X  X  X  X  X  X  X  X  X",
-            ms: "      X           X           X  X     X  X",
+            ls: "X X X X X X X X X",
+            ms: "    X       X       X X   X X",
             hs: "@ms",
-            re: ".  .  X  .  .  .  X  .  .  .  X  .  .  X  X  .",
-            sn: ".  .  X  .  .  .  X  .  .  .  X  .  .  .  X  .",
-            ta: "X           X           X        X  X     X",
-            ag: "                              o  a     a  o "
+            re: ". . X . . . X . . . X . . X X . ",
+            sn: ". . X . . . X . . . X . . . X . ",
+            ta: "X       X       X     X X   X",
+            ag: "                    o a   a o"
         },
         "Break 1": {
+            time: 12,
             ls: "                                          X                                               X                                               X                                               X           X                 X                 X ",
             ms: "@ls",
             hs: "@ls",
@@ -33,6 +33,15 @@ export default {
             ta: "@ls",
             ag: "o   o   o o o o "
         },
+        "4 Hits (Stolen)": {
+            ls: "X X X X ",
+            ms: "@ls",
+            hs: "@ls",
+            re: "@ls",
+            sn: "@ls",
+            ta: "@ls",
+            ag: "o o o o "
+        },
         "Progressive Karla (Stolen)": {
             ls: "X X X X XXXXXXXXXXXXXXXXX       ",
             ms: "@ls",
@@ -43,5 +52,5 @@ export default {
             ag: "o o o o ooooooooooooooooo"
         }
     },
-    exampleSong: ["2/4 Break", "Tune", "Tune", "Break 1", "Tune", "Tune", "Progressive Karla (Stolen)", "Tune", "Tune", "Break 1", "2/4 Break"]
+    exampleSong: ["2/4 Break", "Tune", "Tune", "4 Hits (Stolen)", "Tune", "Tune", "Break 1", "Tune", "Tune", "Progressive Karla (Stolen)", "Tune", "Tune", "2/4 Break"]
 }
