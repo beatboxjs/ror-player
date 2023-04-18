7d20d114278f6d8d96c53fee5fad3caf366986a3
Tim <user@server.fake>
2023-04-18 20:37:55 +0100

    Tune picture links

--- src/ui/overview/about.vue
+++ src/ui/overview/about.vue
@@ -1,5 +1,16 @@
 <div>
     <div class="banner"><h1>Player</h1></div>
-    <div class="content" v-html="content"></div>
+    <div class="content">
+        <div v-html="content"></div>
+        <h1>The Tunes</h1>
+        <div class="styled-links">
+            <router-link 
+                v-for="(tuneName, i) in tuneList"
+                :to="{name: 'listen', params: { tuneName }}"
+                :key="tuneName">
+                {{state.tunes[tuneName].displayName || tuneName}}
+            </router-link>
+        </div>
+    </div>
 </div>
 
