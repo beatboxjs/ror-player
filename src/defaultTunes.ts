e0f3fd090148734bde133df3e25ded1e65c0cebc
Janna Goldstein <janna@impactinstitute.com>
2024-08-21 18:33:17 +0200

    add displayname to hard core break

--- src/defaultTunes.ts
+++ src/defaultTunes.ts
@@ -242,7 +242,8 @@ const rawTunes: {[tuneName: string]: RawTune} = {
 				sh: '@re',
 				ot: '                                                          E D   '
 			},
-			'Hardcore Break': {
+			'Hard Core Break': {
+				displayName: "Hardcore Break",
 				ls: repeat(2, '              XXX             XXX             XXX       XXXXXXXX') +
 					repeat(1, 'X   X   X   X XXX   X   X   X XXX   X   X   X XXX   X   XXXXXXXX') +
 					repeat(1, 'X X X X X X X XXX X X X X X X XXX X X X X X X XXX X X X XXXXXXXX'),
